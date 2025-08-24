e75c265093de53c530cf786417b4f26a963a2284
Eklavya Mathur <eklavyamathurjodhpur@gmail.com>
2025-08-24 15:05:57 +0530

    first

---
+++ src/components/Todo.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef, useState} from 'react'
+import Todoitems from './Todoitems'
+const Todo = () => {
+
+const [TODOLIST, setToDoList] = useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]);
+const inputRef=useRef();
+
+const add=()=>{
+    const inputText=inputRef.current.value.trim();
+    if(inputText===""){
+        return null;
+    }
+    const newToDo={
+        id:Date.now(),
+        text:inputText,
+        isComplete: false,
+    }
+    setToDoList((prev)=>[...prev,newToDo]);
+    inputRef.current.value="";
+
+}
+const deleteTodo =(id)=>{
+    setToDoList((prevTodos)=>{
+        return prevTodos.filter((todo)=>todo.id !==id)
+    })
+
+}
+const toggle=(id)=>{
+    setToDoList((prevTodos)=>{
+        return prevTodos.map((todo)=>{
+            if (todo.id === id){
+                return {...todo,isComplete:!todo.isComplete}
+            }
+            return todo;
+        })
+    })
+}
+useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(TODOLIST));
+},[TODOLIST])
+
+
+
+
+  return (
+    <div  className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+{/*-------------title--------------*/}
+    <div className='flex justify-center gap-2'>
+        <img src="/focusly-logo-by-eklavya.svg" alt="Focusly Logo" className="h-20"/>
+    </div>
+{/*-------------input box--------------*/}
+    <div className='flex items-center my-2 bg-gray-200 rounded-full'>
+        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-11 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+        <button onClick={add} className='border-none rounded-full bg-cyan-500 w-25 h-11 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+    </div>
+{/*-------------TODOLIST--------------*/}
+    <div>
+    {TODOLIST.map((item,index)=>{
+        return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
+    })}
+    </div>
+      
+    </div>
+  )
+}
+
+export default Todo
